af0348d9c985f98fd19fdac2eac6b9a32fc8cc01
Daniel Choi <dhchoi@gmail.com>
2015-12-21 16:09:47 -0500

    time elapsed hoverAndDown

--- HoverDown.elm
+++ HoverDown.elm
@@ -2,11 +2,22 @@ module Main where
 import Graphics.Element exposing (Element, show, flow, down)
 import Graphics.Input exposing (..)
 import Mouse 
-
+import Time exposing (..)
 
 
 clickSignal : Signal.Mailbox ()
 clickSignal = Signal.mailbox ()
+
+
+timer : Signal Time
+timer = every second
+
+holdDownTime : Signal Float
+holdDownTime = Signal.map (Time.inSeconds) 
+    <| Signal.foldp (+) 0 
+    <| Time.fpsWhen 10 hoverAndDown
+    
+
 
 hoverSignal : Signal.Mailbox Bool
 hoverSignal = Signal.mailbox False
@@ -20,17 +31,18 @@ holdDownSig = Signal.sampleOn clickSignal.signal downSignal
 hoverAndDown : Signal Bool
 hoverAndDown = Signal.map2 (&&) hoverSignal.signal downSignal
 
-view : () -> Bool -> Bool -> Bool -> Bool -> Element
-view click downState holdDown hover hoverAndDownState = 
+view : Float -> Bool -> Bool -> Bool -> Bool -> Element
+view time downState holdDown hover hoverAndDownState = 
   flow down [
       show "Hover and click"
-    , show ("Click: " ++ toString click)
     , show ("Down: " ++ toString downState)
     , show ("Hold Down: " ++ toString holdDown)
     , show ("Hover: " ++ toString hover)
     , show ("Hover and Down: " ++ toString hoverAndDownState)
     , hoverable (Signal.message hoverSignal.address) 
         (button (Signal.message clickSignal.address ()) "Click")
+    , show ("Time elapsed hoverAndDown " ++ toString  time)
     ]
 
-main = Signal.map5 view clickSignal.signal downSignal holdDownSig hoverSignal.signal hoverAndDown
+main = Signal.map5 view holdDownTime downSignal holdDownSig hoverSignal.signal hoverAndDown
+              
